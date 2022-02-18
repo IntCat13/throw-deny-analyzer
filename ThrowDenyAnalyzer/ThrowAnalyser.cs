b6ea745513b4d3e3f6623183feb6b68c6de690f7
Rama <Rama@DESKTOP-60G76I7>
2022-02-18 13:01:43 +0100

    Add project files.

---
+++ ThrowDenyAnalyzer/ThrowAnalyser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ThrowDenyAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ThrowAnalyser : DiagnosticAnalyzer
+    {
+        private static DiagnosticDescriptor DiagnosticDescriptor =
+            new DiagnosticDescriptor(
+                "EL0001",
+                "Unaccepted keyword",
+                "Throw is unaccepted keyword, remove to solve",
+                "Clear code",
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> 
+            SupportedDiagnostics => ImmutableArray.Create(DiagnosticDescriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(
+                AnalyzeThrowStatement,
+                SyntaxKind.ThrowStatement);
+
+            context.RegisterSyntaxNodeAction(
+                AnalyzeThrowExpression,
+                SyntaxKind.ThrowExpression);
+        }
+
+        private void AnalyzeThrowStatement(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ThrowStatementSyntax) context.Node;
+
+            context.ReportDiagnostic(
+                Diagnostic.Create(
+                    DiagnosticDescriptor,
+                    node.GetLocation()));
+        }
+
+        private void AnalyzeThrowExpression(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ThrowExpressionSyntax)context.Node;
+
+            context.ReportDiagnostic(
+                Diagnostic.Create(
+                    DiagnosticDescriptor,
+                    node.GetLocation()));
+        }
+    }
+}
